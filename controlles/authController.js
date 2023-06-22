34c28bd55de676e11cc569c5b7b49cd5b96930ee
Mariamtareqq <mariam_tareq22@yahoo.com>
2023-06-22 15:05:00 +0300

    added additional attr to the signed up controller

--- controlles/authController.js
+++ controlles/authController.js
@@ -1,27 +1,26 @@
-const crypto = require('crypto');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User.model');
-const catcAsync = require('../util/catcAsync');
-const AppError = require('../util/AppError');
-const { promisify } = require('util');
-const sendEmail = require('../util/email');
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.model");
+const catcAsync = require("../util/catcAsync");
+const AppError = require("../util/AppError");
+const { promisify } = require("util");
+const sendEmail = require("../util/email");
 
-const signToken = id => {
+const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
+    expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
 
-
 const createAndSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-  
+
   res.status(statusCode).json({
-    status: 'success',
+    status: "success",
     token,
     data: {
-      user
-    }
+      user,
+    },
   });
 };
 exports.signup = catcAsync(async (req, res, next) => {
@@ -29,29 +28,31 @@ exports.signup = catcAsync(async (req, res, next) => {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm ,
-    role: req.body.role 
+    phoneNumber: req.body.phoneNumber,
+    gender: req.body.gender,
+    birthDate: req.body.birthDate,
+    city: req.body.city,
+    role: req.body.role
   });
 
   createAndSendToken(newUser, 201, res);
-  
-}); 
+});
 
 exports.login = catcAsync(async (req, res, next) => {
   const { email, password } = req.body;
   if (!email || !password)
     return next(
       new AppError(
-        'please Make sure that the email and password are provided!',
+        "please Make sure that the email and password are provided!",
         400
       )
     );
 
-  const user = await User.findOne({ email }).select('+password');
+  const user = await User.findOne({ email }).select("+password");
 
   if (!user || !(await user.comparePassowrd(password, user.password)))
     return next(
-      new AppError('please Make sure the email or password are correct !', 401)
+      new AppError("please Make sure the email or password are correct !", 401)
     );
 
   createAndSendToken(user, 201, res);
@@ -64,10 +65,9 @@ exports.protect = catcAsync(async (req, res, next) => {
 
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-
+    req.headers.authorization.startsWith("Bearer")
   ) {
-    token = req.headers.authorization.split(' ')[1];
+    token = req.headers.authorization.split(" ")[1];
   }
 
   if (!token)
@@ -89,29 +89,28 @@ exports.protect = catcAsync(async (req, res, next) => {
   const freshUser = await User.findById(userDecoded.id);
   if (!freshUser) {
     return next(
-      new AppError('the User belonging to this token does not exist', 401)
+      new AppError("the User belonging to this token does not exist", 401)
     );
   }
 
   if (freshUser.changedPasswordAfter(userDecoded.iat))
     return next(
       new AppError(
-        'the User password has recently changed password! please re login',
+        "the User password has recently changed password! please re login",
         401
       )
     );
 
   // grant access
-  req.user = freshUser; 
+  req.user = freshUser;
   next();
 });
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
-    
     if (!roles.includes(req.user.role)) {
       return next(
-        new AppError('this role not allowed to perform this action', 403)
+        new AppError("this role not allowed to perform this action", 403)
       );
     }
 
@@ -131,7 +130,7 @@ exports.forgetPassword = catcAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   const restUrl = `${req.protocol}://${req.get(
-    'host'
+    "host"
   )}/api/v1/users/resetpassword/${resteToken}`;
   const message = `forgot your password submit a PATCH req with your new password and passwrodConfirm to ${restUrl}\n if you did't forget please ignore this email ! `;
 
@@ -139,7 +138,7 @@ exports.forgetPassword = catcAsync(async (req, res, next) => {
     await sendEmail({
       email: req.body.email,
       subject: `youre password reset Token valid for 10 mins`,
-      message
+      message,
     });
   } catch (error) {
     console.error(error);
@@ -148,32 +147,32 @@ exports.forgetPassword = catcAsync(async (req, res, next) => {
     await user.save({ validateBeforeSave: false });
 
     return next(
-      new AppError('there was an error sending the email, try again later', 500)
+      new AppError("there was an error sending the email, try again later", 500)
     );
   }
   res.status(200).json({
-    status: 'succecss',
-    message: 'token sent via email'
+    status: "succecss",
+    message: "token sent via email",
   });
 });
 exports.resetPassword = catcAsync(async (req, res, next) => {
   // 1- get user based on the token
 
   const hashedToken = crypto
-    .createHash('sha256')
+    .createHash("sha256")
     .update(req.params.token)
-    .digest('hex');
+    .digest("hex");
 
   // 2- return user based on the token and check if the token is not expired
 
   const user = await User.findOne({
     passwordRestToken: hashedToken,
-    passwordResetExpiresIn: { $gt: Date.now() }
+    passwordResetExpiresIn: { $gt: Date.now() },
   });
 
   if (!user)
     return next(
-      new AppError('no User with this token or the token has expired', 401)
+      new AppError("no User with this token or the token has expired", 401)
     );
 
   // 3- update changePasswordChangedAt for the user
@@ -189,12 +188,12 @@ exports.resetPassword = catcAsync(async (req, res, next) => {
 
 exports.updatePassword = catcAsync(async (req, res, next) => {
   // 1) get user
-  const user = await User.findById(req.user.id).select('+password');
+  const user = await User.findById(req.user.id).select("+password");
 
   // check if the password is correct
 
   if (!(await user.comparePassowrd(req.body.currentPassword, user.password)))
-    return next(new AppError('password is inccorect please resend it!', 401));
+    return next(new AppError("password is inccorect please resend it!", 401));
 
   user.password = req.body.password;
   user.passwordConfirm = req.body.passwordConfirm;
@@ -202,4 +201,3 @@ exports.updatePassword = catcAsync(async (req, res, next) => {
 
   createAndSendToken(user, 200, res);
 });
-
